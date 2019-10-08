bede9427511df014a441e348ac4ba1ff7ce5ff57
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-10-08 17:21:20 +0900

    StatusBar, TextInput, Dimensions, Platform Update

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar barStyle="light-content" />
+			<Text style={styles.title}>Kawa To Do</Text>
+			<View style={styles.card}>
+				<TextInput style={styles.input} placeholder={'New To Do'} />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#cf6a87',
+		alignItems: 'center',
+	},
+	title: {
+		color: 'white',
+		fontSize: 30,
+		marginTop: 50,
+		fontWeight: '400',
+		marginBottom: 30,
+	},
+	card: {
+		backgroundColor: 'white',
+		flex: 1,
+		width: width - 50,
+		borderTopLeftRadius: 15,
+		borderTopRightRadius: 15,
+		...Platform.select({
+			android: {
+				elevation: 30,
+			},
+		}),
+	},
 });
